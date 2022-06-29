aedea9bd60b5cb628400e823fc26051dcb39f0ac
sedkolsed <sedkolsed@gmail.com>
2022-06-29 21:48:25 +0200

    Express mongoose mongoDB

---
+++ application.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://cedric:keXYejaKahrnAlS2@cluster0.t6ukz.mongodb.net/?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+app.use((req, res, next) => {
+  console.log("Requête reçue !");
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({ message: "Votre requête a bien été reçue !" });
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("Réponse envoyée avec succès !");
+});
+
+module.exports = app;
